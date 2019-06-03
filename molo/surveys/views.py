ff7edde97f0e8315db15db6577555e91cfc7fa76
Codie Roelf <codiebeulaine@gmail.com>
2019-06-03 23:48:48 +0200

    update correct answer

--- molo/surveys/views.py
+++ molo/surveys/views.py
@@ -111,7 +111,7 @@ class ResultsPercentagesJson(View):
 
                 question_stats = results.get(label, {})
                 question_stats[cautious_slugify(answer)] = \
-                    question_stats.get(answer, 0) + 1
+                    question_stats.get(cautious_slugify(answer), 0) + 1
                 results[label] = question_stats
 
         for question, answers in results.items():
